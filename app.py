238d0049022e14ead15cfd86602acfd89101e3c0
Rafael Coderch <rafa.coderch@outlook.es>
2017-12-12 13:55:49 +0100

    Final Project
    Final Final

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[ ]:
+# In[141]:
 
 import dash
 from dash.dependencies import Input, Output 
@@ -10,119 +10,113 @@ import dash_html_components as html
 import pandas as pd
 import plotly.graph_objs as go
 
-#First we import the files and create the coresponding indicators for the values and the countries
 
-#euro_data = pd.read_csv("Eurostat_file.csv")
-euro_data= pd.read_csv("Eurostat_file.csv")
-available_indicators = euro_data['NA_ITEM'].unique()
+# In[143]:
 
-available_countries = euro_data['GEO'].unique()
-
-#We start with creating the app itself
-
+#create the app:
+#app = dash.Dash()
 app = dash.Dash(__name__)
 server = app.server
 app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
+#import the dataset from "http://ec.europa.eu/eurostat/web/products-datasets/-/nama_10_gdp"
+df= pd.read_csv("nama_10_gdp_Data.csv")
 
-#Here I create the dataframe for the units that will be used
+#choose one single unit
+df=df[df['UNIT']=='Current prices, million euro'] 
 
-euro_data1 = euro_data[euro_data['UNIT'] == 'Current prices, million euro']
+#create NA_ITEMS and GEOS
+NA_ITEMS=df.NA_ITEM.unique()
 
-#Now I start designing the layout of my app
+GEOS = df['GEO'].unique()
+
+#FIRST, design the layout of the app
 
 app.layout = html.Div([
 
-#Since we need to put two graphs on the same dashboard this part will be for graph 1    
+#1.The first graph    
 
     html.Div([
-#I create the layout of the first dropdown and set the default value for my graph - Gross domestic product at market prices
+#1.1 the first dropdown box of the first graph (this stuff it is done in the layout section)
         html.Div([
             dcc.Dropdown( 
-#Note that I have given a unique axis name here xaxis-column1, as if I use the same one in both graphs 
-#the dropdown will affect both graphs, I do the same for the yaxis
-                id='xaxis-column1',
-                options=[{'label': i, 'value': i} for i in available_indicators],
-                value='Gross domestic product at market prices'
+                id='xaxisgraph1',
+                options=[{'label': i, 'value': i} for i in NA_ITEMS],
+                value='Final consumption expenditure'
             )
         ],
-        style={'width': '30%', 'display': 'inline-block'}),
-#Here I repeat the same steps as for the x axis, but I put the default value to Wages and Salaries
+        style={'width': '30%', 'display': 'inline-block','color':'red'}),
+#1.2 the second dropdown box for the first graph
         html.Div([
             dcc.Dropdown( 
-                id='yaxis-column1',
-                options=[{'label': i, 'value': i} for i in available_indicators],
-                value='Wages and salaries'
+                id='yaxisgraph1',
+                options=[{'label': i, 'value': i} for i in NA_ITEMS],
+                value='Gross capital formation'
             )
-        ],style={'width': '30%', 'float': 'right', 'display': 'inline-block'})
+        ],style={'width': '30%', 'float': 'right', 'display': 'inline-block','color':'red'})
     ]),
 
-#I give my graph a unique name grph1 in order to be able to have both graphs on my dashboard
-    dcc.Graph(id='grph1'),
-#Finally I create my slider, in order to manipulate it's position I have made it as a html and with style I aligned it with 
-#the graph itself
+#1.3 create the slider
     html.Div(dcc.Slider( 
         id='year--slider',
-        min=euro_data['TIME'].min(),
-        max=euro_data['TIME'].max(),
-        value=euro_data['TIME'].max(),
+        min=df['TIME'].min(),
+        max=df['TIME'].max(),
+        value=df['TIME'].min(),
         step=None,
-        marks={str(time): str(time) for time in euro_data['TIME'].unique()},
+        marks={str(time): str(time) for time in df['TIME'].unique()},
+        
+    ), style={'marginRight': 50, 'marginLeft': 190, 'marginTop':50, 'marginBottom':50},),
     
-    ), style={'marginRight': 50, 'marginLeft': 110},),
+#1.4 Create the first graph itself
+    dcc.Graph(id='graph1'),
 
-#Here I start creating the environment for my second chart folowing the same steps as I did for my previous one with some minor
-#alterations
+#2. The second graph
     
     html.Div([
-        
-        html.Div([
+#2.1 The first dropdownbox of the second graph        
+           html.Div([
             dcc.Dropdown( 
-#Note how here I use xaxis-column2 as my id to prevent the dropdown from my previous graph to interact with this 
-#axis and vice-versa
-                id='xaxis-column2',
-                options=[{'label': i, 'value': i} for i in available_indicators],
-                value='Gross domestic product at market prices'
+
+                id='xaxisgraph2',
+                options=[{'label': i, 'value': i} for i in NA_ITEMS],
+                value='Final consumption expenditure'
             )
         ],
-        style={'width': '30%', 'marginTop': 40, 'display': 'inline-block'}),
-# The bigest change from the previous chart is here, where I use available_countries as the option for my dropdown
-# and I set Spain as the default value
+        style={'width': '30%', 'marginTop': 40, 'display': 'inline-block','color':'red'}),
+#2.2 The second dropdownbox of the second graph
         html.Div([
             dcc.Dropdown( 
-                id='yaxis-column2',
-                options=[{'label': i, 'value': i} for i in available_countries],
+                id='yaxisgraph2',
+                options=[{'label': i, 'value': i} for i in GEOS],
                 value= "Spain"
                 
             )
-        ],style={'width': '30%', 'marginTop': 40, 'float': 'right', 'display': 'inline-block'})
+        ],style={'width': '30%', 'marginTop': 40, 'float': 'right', 'display': 'inline-block','color':'red'})
      ]),
-# Finally I give my second graph a unique id graph2
-     dcc.Graph(id='grph2'),
+#2.3 create the second graph itself
+     dcc.Graph(id='graph2'),
 
 
 ])
 
-#Here I create the callback which updates the first graph according to the value in the fropdown boxes
-#I start by setting my imputs and outputs
+#3. Create the callback which updates the first graph according to the value in the dropdown boxes
+
 @app.callback(
-    dash.dependencies.Output('grph1', 'figure'),
-    [dash.dependencies.Input('xaxis-column1', 'value'),
-     dash.dependencies.Input('yaxis-column1', 'value'),
+    dash.dependencies.Output('graph1', 'figure'),
+    [dash.dependencies.Input('xaxisgraph1', 'value'),
+     dash.dependencies.Input('yaxisgraph1', 'value'),
      dash.dependencies.Input('year--slider', 'value')])
 
-#Then I define the function
-def update_graph(xaxis_column_name, yaxis_column_name,
+#4. Define the function for the first graph
+def update_graph1(xaxis_column_name, yaxis_column_name,
                  year_value):
-
-#I create the dataframe which will match the coresponding value with the year that is chosen
-    euro_data_yearly = euro_data[euro_data['TIME'] == year_value]
-#And finally I create the output, in addition to the values on the x and y axis, I add the text marker which shows the country,
-#when you hover over the coresponding scatter value with the cursor
+#4.1 create the dataset with the year chosen 
+    year_data = df[df['TIME'] == year_value]
+#4.2 create the data with the inputs of the dropdown boxes.
     return {
         'data': [go.Scatter(
-            x=euro_data_yearly[euro_data_yearly['NA_ITEM'] == xaxis_column_name]['Value'],
-            y=euro_data_yearly[euro_data_yearly['NA_ITEM'] == yaxis_column_name]['Value'],
-            text=euro_data_yearly[euro_data_yearly['NA_ITEM'] == yaxis_column_name]['GEO'],
+            x=year_data[year_data['NA_ITEM'] == xaxis_column_name]['Value'],
+            y=year_data[year_data['NA_ITEM'] == yaxis_column_name]['Value'],
+            hovertext=year_data[year_data['NA_ITEM'] == yaxis_column_name]['GEO'],
             mode='markers',
             marker={
                 'size': 15,
@@ -139,29 +133,27 @@ def update_graph(xaxis_column_name, yaxis_column_name,
                 'title': yaxis_column_name,
                 'type': 'linear'
             },
-# I set the margins for both of the charts to be the same for a neater look of my dashboard
-            margin={'l': 110, 'b': 50, 't': 20, 'r': 50},
+            margin={'l': 90, 'b': 50, 't': 20, 'r': 50},
             hovermode='closest'
         )
     }
 
-#Here I create the call back for the second chart, starting with defining the input and output using the id's I have assigned
-#to my second graph and the coresponding ones for the x and y axis
+##5. Create the callback which updates the second graph according to the value in the dropdown boxes
 @app.callback(
-    dash.dependencies.Output('grph2', 'figure'),
-    [dash.dependencies.Input('xaxis-column2', 'value'),
-     dash.dependencies.Input('yaxis-column2', 'value')])
-#As here I have all of the years I just have to update the column names of the chart
-def update_graph(xaxis_column_name, yaxis_column_name):
-#I create a dataframe which will mach the country or country group with the coresponding x axis value    
-    euro_data_yearly = euro_data1[euro_data1['GEO'] == yaxis_column_name]
+    dash.dependencies.Output('graph2', 'figure'),
+    [dash.dependencies.Input('xaxisgraph2', 'value'),
+     dash.dependencies.Input('yaxisgraph2', 'value')])
 
-#Finally I create my output, using mode='lines' to create a linechart. Note that here I do not need text= as there is nothing to
-#label on the chart area itself, as it will show values for only the country chosen in the dropdown
+#6. Define the function for the second graph
+def update_graph2(xaxis_column_name, yaxis_column_name):
+#6.1 create the dataset with the country chosen     
+    country_data = df[df['GEO'] == yaxis_column_name]
+
+#6.2 create the linechart
     return {
         'data': [go.Scatter(
-            x=euro_data_yearly['TIME'].unique(),
-            y=euro_data_yearly[euro_data_yearly['NA_ITEM'] == xaxis_column_name]['Value'],
+            x=country_data['TIME'].unique(),
+            y=country_data[country_data['NA_ITEM'] == xaxis_column_name]['Value'],
             mode='lines',
             marker={
                 'size': 15,
@@ -178,13 +170,18 @@ def update_graph(xaxis_column_name, yaxis_column_name):
                 'title': yaxis_column_name,
                 'type': 'linear'
             },
-            margin={'l': 110, 'b': 50, 't': 20, 'r': 50},
+            margin={'l': 90, 'b': 50, 't': 20, 'r': 50},
             hovermode='closest'
         )
     }
 
-#Finally I run the server for my dashboard
+#7.Run the server
 
 if __name__ == '__main__':
     app.run_server()
+
+
+# In[ ]:
+
+
 
